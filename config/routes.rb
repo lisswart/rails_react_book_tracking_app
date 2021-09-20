ff7b7cc082e6d9b543fe8b67141e7ba74e15b63b
Lisa Swart <arnoudlisa@gmail.com>
2021-09-20 13:13:47 -0400

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/hello", to: "application#hello_world"
+
+  namespace :api do
+    post '/signup', to: 'users#create'
+    get '/me', to: 'users#show'
+
+    post '/login', to: 'sessions#create'
+    delete '/logout', to: 'sessions#destroy'
+
+    post '/books', to: 'books#create'
+    get '/books', to: 'books#index'
+    get '/books/:id', to: 'books#show'
+    patch '/books/:id', to: 'books#update'
+    delete '/books/:id', to: 'books#destroy'
+  end
+
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
